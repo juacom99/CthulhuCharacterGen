6329c67ec26c2420d076327998f856cbc2018905
juacom99 <juacom04@gmail.com>
2019-12-30 17:57:42 -0300

    Character properties added

--- src/main/java/com/improvisados/cthulhucharactergen/Main.java
+++ src/main/java/com/improvisados/cthulhucharactergen/Main.java
@@ -24,17 +24,17 @@ public class Main
         
         //JDiceRoller.add( roller.roll(3,6))
         
-        int STR=45;//JDiceRoller.add( roller.roll(3,6));
+        int STR=50;//JDiceRoller.add( roller.roll(3,6));
         int CON=JDiceRoller.add( roller.roll(3,6))*5;
         int SIZ=50;//JDiceRoller.add( roller.roll(2,6))+6;
-        int DEX=45;//JDiceRoller.add( roller.roll(3,6));
+        int DEX=50;//JDiceRoller.add( roller.roll(3,6));
         int APP=JDiceRoller.add( roller.roll(3,6))*5;
         int INT=(JDiceRoller.add( roller.roll(2,6))+6)*5;
         int POW=JDiceRoller.add( roller.roll(3,6))*5;
         int EDU=(JDiceRoller.add( roller.roll(2,6))+6)*5;
         int LUK=JDiceRoller.add( roller.roll(3,6))*5;
         
-        Character cha=new Character("Prueba",STR,CON,SIZ,DEX,APP,INT,POW,EDU,LUK);
+        Character cha=new Character("Prueba","juacom99",45,Sex.MALE,"Montevideo","Montevideo",STR,CON,SIZ,DEX,APP,INT,POW,EDU,LUK);
         System.out.println(cha);
            
     }
